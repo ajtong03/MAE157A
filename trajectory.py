3c1caa577689a3c283dae7e712f1ccf8a1d99218
idalyj <idalyj@g.ucla.edu>
2025-04-19 07:10:20 -0700

    Update trajectory.py

--- trajectory.py
+++ trajectory.py
@@ -1,41 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def solve_polynomial_coefficients(t_f, p0, v0, a0, pf, vf, af):
+def solve_polynomial_coefficients(t_f, p0, v0, a0, j0, pf, vf, af, jf):
 # 7th order polynomial
-
     A = np.array([
-        [1, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 0, 2, 0, 0, 0],
-        [1, t_f, t_f**2, t_f**3, t_f**4, t_f**5],
-        [0, 1, 2 * t_f, 3 * t_f**2, 4 * t_f**3, 5 * t_f**4],
-        [0, 0, 2, 6 * t_f, 12 * t_f**2, 20 * t_f**3]
-        # [0, 0, 0, 6, 24*t_f, 60*t_f**2]
-        # [0, 0, 0, 0 , 24,120*t_f]
+        [1, 0, 0, 0, 0, 0, 0, 0], # c_0 = p0
+        [0, 1, 0, 0, 0, 0, 0, 0], # c_1 = v0
+        [0, 0, 2, 0, 0, 0, 0, 0], # c_2 = a0/2
+        [0, 0, 0, 6, 0, 0, 0, 0], # c_3 = j0/6
+        [1, t_f, t_f**2, t_f**3, t_f**4, t_f**5, t_f**6, t_f**7],
+        [0, 1, 2 * t_f, 3 * t_f**2, 4 * t_f**3, 5 * t_f**4, 6 * t_f**5, 7 * t_f**6],
+        [0, 0, 2, 6 * t_f, 12 * t_f**2, 20 * t_f**3, 30 * t_f**4, 42 * t_f**5],
+        [0, 0, 0, 6, 24 * t_f, 60 * t_f**2, 120 * t_f**3, 210 * t_f**4]
+        
     ])
-    b = np.array([p0, v0, a0, pf, vf, af])
+    b = np.array([p0, v0, a0, j0, pf, vf, af, jf])
     c = np.linalg.solve(A, b)
     return c
 
 # Approach Segment 
 # Goal is to have drone go through origin (gate location)
-#Recommended to have 0 acceleration through the gate
+# Recommended to have 0 acceleration through the gate and thrust perpendicular to gate's side
 # mass_drone = 0.399 kg
-# X-DIRECTION 
-tf = 15  # seconds
-c_x = solve_polynomial_coefficients(tf, 1.5, 0, 0, 0, 3, 0) #BCs
-print("x-Coeffs:", c_x)
 
+# x - dire 
+tf = 12  # seconds
+c_x = solve_polynomial_coefficients(tf, 1.5,0 ,0, 0, 0, 2, 0, 0 )
 def x_t(t):
-    return c_x[0] + c_x[1] * t + c_x[2] * t**2 + c_x[3] * t**3 + c_x[4] * t**4 + c_x[5] * t**5
-
+    return c_x[0] + c_x[1] * t + c_x[2] * t**2 + c_x[3] * t**3 + c_x[4] * t**4 + c_x[5] * t**5 + c_x[6] * t**6 + c_x[7] * t**7
 def vx_t(t):
-    return c_x[1] + 2 * c_x[2] * t + 3 * c_x[3] * t**2 + 4 * c_x[4] * t**3 + 5 * c_x[5] * t**4
+    return c_x[1] + 2 * c_x[2] * t + 3 * c_x[3] * t**2 + 4 * c_x[4] * t**3 + 5 * c_x[5] * t**4 + 6 * c_x[6] * t**5 + 7 * c_x[7] * t**6
 
 def ax_t(t):
-    return 2 * c_x[2] + 6 * c_x[3] * t + 12 * c_x[4] * t**2 + 20 * c_x[5] * t**3
+    return 2 * c_x[2] + 6 * c_x[3] * t + 12 * c_x[4] * t**2 + 20 * c_x[5] * t**3 + 30 * c_x[6] * t**4 + 42 * c_x[7] * t**5 
 
+def jx_t(t):
+    return 6 * c_x[3] + 24 * c_x[4] * t + 60 * c_x[5] * t**2 + 120 * c_x[6] * t**3 + 210 * c_x[7] * t**4
+
+# y - dir
 
 # Plotting
 time_values = np.linspace(0, tf, 200)
