108f499cc4bfda7ce18e3e9e62bcf858f601ea80
idalyj <idalyj@g.ucla.edu>
2025-04-20 15:27:06 -0700

    Update trajectory.py

--- trajectory.py
+++ trajectory.py
@@ -46,6 +46,7 @@ plt.figure(figsize=(8, 6))
 plt.plot(time_values, [x_t(t) for t in time_values], label="Position")
 plt.plot(time_values, [vx_t(t) for t in time_values], label="Velocity")
 plt.plot(time_values, [ax_t(t) for t in time_values], label="Acceleration")
+plt.plot(time_values, [jx_t(t) for t in time_values], label = "Jerk")
 plt.title("X-Direction Trajectory (approach segment)")
 plt.xlabel("Time (s)")
 plt.ylabel("Value")
@@ -54,3 +55,62 @@ plt.grid(True)
 plt.show()
 
 # y - dir
+c_y = solve_polynomial_coefficients(tf, -1 ,0 ,0, 0, 0, 2, 0, 0 )
+# assuming frame is position 1 m from the ground
+def y_t(t):
+    return c_y[0] + c_y[1] * t + c_y[2] * t**2 + c_y[3] * t**3 + c_y[4] * t**4 + c_y[5] * t**5 + c_y[6] * t**6 + c_y[7] * t**7
+def vy_t(t):
+    return c_y[1] + 2 * c_y[2] * t + 3 * c_y[3] * t**2 + 4 * c_y[4] * t**3 + 5 * c_y[5] * t**4 + 6 * c_y[6] * t**5 + 7 * c_y[7] * t**6
+
+def ay_t(t):
+    return 2 * c_y[2] + 6 * c_y[3] * t + 12 * c_y[4] * t**2 + 20 * c_y[ 5] * t**3 + 30 * c_x[6] * t**4 + 42 * c_x[7] * t**5 
+
+def jy_t(t):
+    return 6 * c_y[3] + 24 * c_y[4] * t + 60 * c_y[5] * t**2 + 120 * c_y[6] * t**3 + 210 * c_x[7] * t**4
+
+
+# Plotting
+time_values = np.linspace(0, tf, 200)
+plt.figure(figsize=(8, 6))
+plt.plot(time_values, [y_t(t) for t in time_values], label="Position")
+plt.plot(time_values, [vy_t(t) for t in time_values], label="Velocity")
+plt.plot(time_values, [ay_t(t) for t in time_values], label="Acceleration")
+plt.title("Y-Direction Trajectory (approach segment)")
+plt.xlabel("Time (s)")
+plt.ylabel("Value")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# z - dir 
+c_z = solve_polynomial_coefficients(tf, -1 ,0 ,0, 0, 0, 2, 0, 0 )
+# remember acceleration <==> orientation/thrust (yaw in z-dir rotates plane left and right)
+def z_t(t):
+    return c_z[0] + c_z[1] * t + c_z[2] * t**2 + c_z[3] * t**3 + c_z[4] * t**4 + c_z[5] * t**5 + c_z[6] * t**6 + c_z[7] * t**7
+def vz_t(t):
+    return c_z[1] + 2 * c_z[2] * t + 3 * c_z[3] * t**2 + 4 * c_z[4] * t**3 + 5 * c_z[5] * t**4 + 6 * c_z[6] * t**5 + 7 * c_z[7] * t**6
+
+def az_t(t):
+    return 2 * c_z[2] + 6 * c_z[3] * t + 12 * c_z[4] * t**2 + 20 * c_z[ 5] * t**3 + 30 * c_x[6] * t**4 + 42 * c_z[7] * t**5 
+
+def jz_t(t):
+    return 6 * c_z[3] + 24 * c_z[4] * t + 60 * c_z[5] * t**2 + 120 * c_z[6] * t**3 + 210 * c_z[7] * t**4
+
+
+# Plotting
+time_values = np.linspace(0, tf, 200)
+plt.figure(figsize=(8, 6))
+plt.plot(time_values, [z_t(t) for t in time_values], label="Position")
+plt.plot(time_values, [vz_t(t) for t in time_values], label="Velocity")
+plt.plot(time_values, [az_t(t) for t in time_values], label="Acceleration")
+plt.title("Z-Direction Trajectory (approach segment)")
+plt.xlabel("Time (s)")
+plt.ylabel("Value")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# plotting in 3D
+
+
+
