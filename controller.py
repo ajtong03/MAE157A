616f6bbdfd4e942ac16403e094c0aed9913e69cd
ajtong03 <alissatong@ucla.edu>
2025-04-23 10:42:00 -0700

    controller and dynamics updated
    - use naiveComputeGains for now
    - dynamics init function updated with proper inertia tensor, moment arm, and drag coefficient

--- controller.py
+++ controller.py
@@ -29,8 +29,34 @@ def overshoot_settime(q_act, q_d, time = 10, thresh = 0.05):
             set_time = time[i]
             break
     return overshoot, set_time
- 
 
+# naive approach to computing gains  
+def naiveComputeGains(q_act, q_d):
+    Kp_range = (1, 20)  # roll, pitch, yaw
+    Kd_range = (1, 20)
+
+    q_e = quaternionfunc.error(q_act, q_d)
+    q_e_opt = q_e
+
+    # if q_act and q_d are the same, there should be no gains applied
+    Kp_opt = np.diag([0, 0, 0])
+    Kd_opt = np.diag([0, 0, 0])
+
+    for i in range(1000):
+        Kp = np.diag([random.uniform(*Kp_range) for i in range(3)])
+        Kd = np.diag([random.uniform(*Kd_range) for i in range(3)]) 
+
+        #call function to get q_act
+        #q_new = _____
+        qe_new = quaternionfunc(q_new, q_d)
+        if(qe_new < q_e_opt):
+            q_e_opt = qe_new
+            Kp_opt = Kp
+            Kd_opt = Kd
+    
+    return Kp_opt, Kd_opt
+
+# the torque needs to be applied to the orientation and q_act needs to be refed into this   
 def computeGains(q_act, q_d):
     # starting gain ranges
     Kp_range = (1, 20)  # roll, pitch, yaw
@@ -38,14 +64,15 @@ def computeGains(q_act, q_d):
     # REASONABLE RANGE FOR LAMBDA=?
     lambda_range = (0, 1)
 
+    # set these so that if q_act is already at q_d, stay in orientation
     Kp_opt = np.diag([1, 1, 1])
     Kd_opt = np.diag([1, 1, 1])
     lambda_opt = np.diag([1, 1, 1])
 
     #set variable to track optimal performance value
-    perf_opt = float('inf')
-    over_opt = float('inf')
-    set_opt = float('inf') 
+    #q_e = quaternionfunc.error(q_act, q_d)
+    over_opt, set_opt = overshoot_settime(q_act, q_d)
+    perf_opt = over_opt + set_opt
 
     #run 1000 iterations to tune Kp and Kd
     for i in range(1000):
@@ -54,8 +81,6 @@ def computeGains(q_act, q_d):
         Kp = np.diag([random.uniform(*Kp_range) for i in range(3)])
         Kd = np.diag([random.uniform(*Kd_range) for i in range(3)])
         lambda_mat = np.diag([random.uniform(*lambda_range)] for i in range(3))
-
-        # q_e = quaternionfunc.error(q_act, q_d)
 
         #performance parameters
         overshoot, set_time = overshoot_settime(q_act, q_d)
@@ -74,9 +99,9 @@ def computeGains(q_act, q_d):
             Kd_range = (Kd_opt[0, 0] - 0.5, Kd_opt[0, 0] + 0.5)
         
         #Increase the range if no improvement
-        elif i % 100 == 0 and perf_opt == float('inf'):
-            Kp_range = (0.8*Kp_range(0), 1.2*Kp_range(1))
-            Kd_range = (0.8*Kd_range(0), 1.2*Kd_range(1))
+        # elif i % 100 == 0 and perf_opt == float('inf'):
+        #     Kp_range = (0.8*Kp_range(0), 1.2*Kp_range(1))
+        #     Kd_range = (0.8*Kd_range(0), 1.2*Kd_range(1))
     
     return Kp_opt, Kd_opt, over_opt, set_opt, lambda_opt
 
@@ -92,18 +117,17 @@ def computeTorqueNaive(Kp, Kd, q_act, q_d, w, w_d):
     torque = -q_e(0) * Kp * q_e(1) - Kd * w_e
     return torque
 
-def computeTorque(Kp, Kd, lambda_opt, q_act, q_d, w, w_d):
-    # REPLACE WITH ACTUAL VALUE OF Re
-    Re = 1
+def computeTorque(Kp, Kd, lambda_opt, q_act, q_d, w, w_d, Re):
+    # Re should be computed in main using dynamics.quat_to_rot(q_e)
 
-    q_e = quaternionfunc.error(q_act,q_d)
+    qe_mag, qe_vec = quaternionfunc.error(q_act,q_d)
     w_e = w - Re * w_d
     q_deriv = quaternionfunc.deriv(q_act, w)
 
     sgn = 1
-    if(q_e <= 0):
+    if(qe_mag < 0):
         sgn = -1
 
-    torque = -sgn * q_e(0) * Kp * q_e(1) - Kd * w_e - lambda_opt * sgn * q_e(0) * q_deriv(1)
+    torque = -sgn * qe_mag * Kp * qe_vec - Kd * w_e - lambda_opt * sgn * qe_mag * q_deriv(1)
     return torque
     
