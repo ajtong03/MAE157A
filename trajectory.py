9f03111f813ecc52f336a6ce0e5584099db72f39
idalyj <idalyj@g.ucla.edu>
2025-04-29 03:22:39 -0700

    Update trajectory.py
    Better boundary conditions/all directional trajectories

--- trajectory.py
+++ trajectory.py
@@ -4,15 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from math import sin,cos, tan
 from dynamics import dynamics   # import the class, not the module
 import matplotlib.animation as animation
-
 import quaternionfunc 
-# get desired rotation matrix
-def desired_rotation_matrix(Td, ad):
-    Td = Td/np.linalg.norm(Td)
-    ad = ad /np.linalg.norm(ad)
-Td = np.array([0,0,-1])
-
-
 
 def solve_polynomial_coefficients(t_f, p0, v0, a0, j0, pf, vf, af, jf):
 # 7th order polynomial
@@ -56,7 +48,7 @@ def jx_t(t):
 
 # y - dir
 # focus on y accel (make sure orientation lines up)
-c_y = solve_polynomial_coefficients(tf, -1.5 , 0 ,0, 0, 0, 1.4142,-4.33, 0 )
+c_y = solve_polynomial_coefficients(tf, -1.5 , 0 ,0, 0, 0, 1.4142,0.5, 0 )
 print("y-coeffs:", c_y)
 def y_t(t):
     return c_y[0] + c_y[1] * t + c_y[2] * t**2 + c_y[3] * t**3 + c_y[4] * t**4 + c_y[5] * t**5 + c_y[6] * t**6 + c_y[7] * t**7
@@ -89,12 +81,12 @@ def jz_t(t):
 
 
 # Departure Segment 
-# Initial Boundary Conditions must must final BCs from approach segment
-tf1 =  8# seconds
+# Initial Boundary Conditions must match final BCs from approach segment
+tf1 =  5 # seconds
 time_departure = np.linspace(0, tf1, 200)
 
 # x - axis
-c_x1 = solve_polynomial_coefficients(tf1, 0, 1.25, 0.5, 0, 1.5, 0, 0, 0 )
+c_x1 = solve_polynomial_coefficients(tf1, 0, 1.25, 0.5, 0, -1.5, 0, 0, 0 )
 #
 print("x-coeffs:", c_x1)
 def x_t1(t):
@@ -110,7 +102,7 @@ def jx_t1(t):
 
 
 # y - dir
-c_y1 = solve_polynomial_coefficients(tf1, 0 , 1.4142 ,0, 0, 1.5, 0, 0, 0 )
+c_y1 = solve_polynomial_coefficients(tf1, 0 , 1.4142 ,0.5, 0, 1.5, 0, 0, 0 )
 print("y-coeffs:", c_y1)
 def y_t1(t):
     return c_y1[0] + c_y1[1] * t + c_y1[2] * t**2 + c_y1[3] * t**3 + c_y1[4] * t**4 + c_y1[5] * t**5 + c_y1[6] * t**6 + c_y1[7] * t**7
@@ -128,7 +120,7 @@ def jy_t1(t):
 c_z1 = solve_polynomial_coefficients(tf1, 1, 0 ,0, 0, 0, 0, 0, 0 )
 print("z-coeffs:", c_z1)
 # remember acceleration <==> orientation/thrust (yaw in z-dir rotates plane left and right)
-# needs to counteract gravity in this case
+# needs to counteract gravity in this case 
 def z_t1(t):
     return c_z1[0] + c_z1[1] * t + c_z1[2] * t**2 + c_z1[3] * t**3 + c_z1[4] * t**4 + c_z1[5] * t**5 + c_z1[6] * t**6 + c_z1[7] * t**7
 def vz_t1(t):
@@ -146,22 +138,22 @@ def jz_t1(t):
 
 # x - dir
 plt.figure(figsize=(8, 6))
-plt.plot(time_approach, [x_t(t) for t in time_approach], label="Position")
-plt.plot(time_approach, [vx_t(t) for t in time_approach], label="Velocity")
+plt.plot(time_departure, [x_t(t) for t in time_departure], label="Position")
+plt.plot(time_departure, [vx_t(t) for t in time_departure], label="Velocity")
 #plt.plot(time_values, [ax_t(t) for t in time_approach], label="Acceleration")
 #plt.plot(time_values, [jx_t(t) for t in time_values], label = "Jerk")
-plt.title("X-Direction Trajectory (approach segment)")
+plt.title("X-Direction Trajectory (departure segment)")
 plt.xlabel("Time (s)")
 plt.ylabel("Value")
 plt.legend()
 plt.grid(True)
 plt.show()
-'''
+
 # y-dir
 plt.figure(figsize=(8, 6))
-plt.plot(time_values, [y_t(t) for t in time_values], label="Position")
-plt.plot(time_values, [vy_t(t) for t in time_values], label="Velocity")
-plt.plot(time_values, [ay_t(t) for t in time_values], label="Acceleration")
+plt.plot(time_departure, [y_t(t) for t in time_departure], label="Position")
+plt.plot(time_departure, [vy_t(t) for t in time_departure], label="Velocity")
+plt.plot(time_departure, [ay_t(t) for t in time_departure], label="Acceleration")
 plt.title("Y-Direction Trajectory (approach segment)")
 plt.xlabel("Time (s)")
 plt.ylabel("Value")
@@ -172,16 +164,15 @@ plt.show()
 # z-dir
 
 plt.figure(figsize=(8, 6))
-plt.plot(time_values, [z_t(t) for t in time_values], label="Position")
-plt.plot(time_values, [vz_t(t) for t in time_values], label="Velocity")
-plt.plot(time_values, [az_t(t) for t in time_values], label="Acceleration")
-plt.title("Z-Direction Trajectory (approach segment)")
+plt.plot(time_departure, [z_t(t) for t in time_departure], label="Position")
+plt.plot(time_departure, [vz_t(t) for t in time_departure], label="Velocity")
+plt.plot(time_departure, [az_t(t) for t in time_departure], label="Acceleration")
+plt.title("Z-Direction Trajectory (departure segment)")
 plt.xlabel("Time (s)")
 plt.ylabel("Value")
 plt.legend()
 plt.grid(True)
 plt.show()
-'''
 
 
 # plotting in 3D
@@ -193,10 +184,28 @@ x_traj_departure = [x_t1(t) for t in time_departure]
 y_traj_departure = [y_t1(t) for t in time_departure]
 z_traj_departure = [z_t1(t) for t in time_departure]
 
-#use this in main code to combine all polynomials 
+## working on getting desired orientation using the lecture notes
+'''
+def get_desired_orientation (theta, a_d)
+Rd = x_t + x_t1 + y_t + y_t1 + z_t + az_t1
+ad = ax_t + ax_t1 + ay_t + ay_t1 + az_t + ay_t1 + g
+    Td = Td/np.linalg.norm(Td)
+    ad = ad /np.linalg.norm(ad)
+    ad = 1/m * 
+    T_d = m * np.linalg.norm(ad)
+    n_vec
+
+Td = np.array([0,0,-1])
+'''
+
+
+
+#use this in main code to combine all polynomials, not sure if I wrote this function correctly lol 
 x_full_traj = x_traj_approach + x_traj_departure 
 y_full_traj = y_traj_approach + y_traj_departure 
 z_full_traj = z_traj_approach + z_traj_departure
+
+
 
 
 # 3D Plot
